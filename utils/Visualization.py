ffc92d5797f758e8768f425f36aff3246d8e15c7
안홍현 <hyunynim1@naver.com>
2020-07-09 21:03:06 +0900

    Make jupyter notebook file for visualization
    Visualization.py doesnt work

--- utils/Visualization.py
+++ utils/Visualization.py
@@ -2,15 +2,15 @@ import ipyvolume as ipv
 import h5py
 import numpy as np
 
-file_name = '../0.000.h5'
+file_name = '../model_normalized_64.h5'
 model = h5py.File(file_name, 'r')
 data = np.array(model['data'][:])
 X = data[:,:,:,0]
-#Y = data[:,:,:,1]
-#Z = data[:,:,:,2]
+Y = data[:,:,:,1]
+Z = data[:,:,:,2]
 
 fig = ipv.figure()
 ipv.volshow(X, controls=False, max_shape=64)
-#ipv.volshow(Y, controls=False, max_shape=64)
-#ipv.volshow(Z, controls=False, max_shape=64)
+ipv.volshow(Y, controls=False, max_shape=64)
+ipv.volshow(Z, controls=False, max_shape=64)
 ipv.show()
